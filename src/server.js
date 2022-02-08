d1c452d30a0504a3b3e86e5cb1c4fc2298af6e15
aiden <zero2one23581@gmail.com>
2022-02-08 17:35:56 +0900

    get, middleware(morgan)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
+//EXPRESS APPLICATION
 const app = express();
+const logger = morgan("dev");
 
+//CONTROLLER
+const home = (req, res) => {
+  res.send("home");
+};
+//MIDDLEWARE
+app.use(logger);
+
+//GET REQUEST
+///when somebody(Browser) reqests to get '/' page => activates 'home' function
+app.get("/", home);
+
+//SERVER LISTENING
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`✅ Aiden's server listening on the port http://localhost:${PORT} 🔥`);
