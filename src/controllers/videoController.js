9e9149b4f3d0ee814f08ac44ba147a680f19b4e1
aiden <zero2one23581@gmail.com>
2022-02-08 18:42:40 +0900

    url parameters
    req.params

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,13 @@
+//rootRouter
 export const home = (req, res) => {
   res.send("HOME");
 };
 export const search = (req, res) => {
   res.send("SEARCH VIDEO");
 };
+//videoRouter
 export const watch = (req, res) => {
+  console.log(req.params);
   res.send("WATCH VIDEO");
 };
 export const upload = (req, res) => {
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,9 +2,9 @@ import express from "express";
 import { upload, watch, editVideo, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
-videoRouter.route("/").get(watch);
-videoRouter.route("/upload").get(upload);
-videoRouter.route("/edit").get(editVideo);
-videoRouter.route("/delete").get(deleteVideo);
+videoRouter.route("/:id").get(watch);
+videoRouter.route("/:id/upload").get(upload);
+videoRouter.route("/:id/edit").get(editVideo);
+videoRouter.route("/:id/delete").get(deleteVideo);
 
 export default videoRouter;
