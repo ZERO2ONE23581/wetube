82b4bb68025e9ae539f11d73788089f258d4fbb5
aiden <zero2one23581@gmail.com>
2022-02-08 18:16:15 +0900

    routers, controllers

---
+++ src/README.md
@@ -0,0 +1,14 @@
+# Wetube Reloaded
+
+/ -> HOME(메인홈)
+/join -> 회원가입
+/login -> 로그인
+/search -> 동영상찾기
+
+C /videos/upload -> Upload video
+R /videos/watch -> Watch video
+U /videos/edit -> Edit video
+D /videos/delete -> Delete video
+
+/users/edit -> Edit user
+/users/delete -> Delete user
--- src/server.js
+++ src/server.js
@@ -5,16 +5,64 @@ import morgan from "morgan";
 const app = express();
 const logger = morgan("dev");
 
-//CONTROLLER
+//CONTROLLERS
 const home = (req, res) => {
-  res.send("home");
+  res.send("HOME");
 };
+const join = (req, res) => {
+  res.send("JOIN");
+};
+const login = (req, res) => {
+  res.send("LOGIN");
+};
+const search = (req, res) => {
+  res.send("SEARCH VIDEO");
+};
+const watch = (req, res) => {
+  res.send("WATCH VIDEO");
+};
+const upload = (req, res) => {
+  res.send("UPLOAD VIDEO");
+};
+const editVideo = (req, res) => {
+  res.send("EDIT VIDEO");
+};
+const deleteVideo = (req, res) => {
+  res.send("DELETE VIDEO");
+};
+const editUser = (req, res) => {
+  res.send("EDIT USER");
+};
+const deleteUser = (req, res) => {
+  res.send("DELETE USER");
+};
+
+//ROUTERS
+//("/")
+const rootRouter = express.Router();
+rootRouter.route("/").get(home);
+rootRouter.route("/join").get(join);
+rootRouter.route("/login").get(login);
+rootRouter.route("/search").get(search);
+
+//("/videos")
+const videoRouter = express.Router();
+videoRouter.route("/upload").get(upload);
+videoRouter.route("/watch").get(watch);
+videoRouter.route("/edit").get(editVideo);
+videoRouter.route("/delete").get(deleteVideo);
+
+//("/users")
+const userRouter = express.Router();
+userRouter.route("/edit").get(editUser);
+userRouter.route("/delete").get(deleteUser);
+
 //MIDDLEWARE
 app.use(logger);
-
-//GET REQUEST
-///when somebody(Browser) reqests to get '/' page => activates 'home' function
-app.get("/", home);
+//CONNECT ROUTERS TO EXPRESS APP
+app.use("/", rootRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 //SERVER LISTENING
 const PORT = 4000;
