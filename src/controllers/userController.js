5724446132338c7ac2484722b7144109f7759f21
aiden <zero2one23581@gmail.com>
2022-02-10 00:18:08 +0900

    create 2 + hash pw

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,25 @@
+import User from "../models/User";
+
 ///Root Router
 //회원가입 Join (Create)
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
 };
-export const postJoin = (req, res) => {
-  console.log(req.body);
-  return res.end();
+export const postJoin = async (req, res) => {
+  const { email, username, password, name, location } = req.body; //1. receive data from join page
+  await User.create({
+    //2. create new User model
+    email,
+    username,
+    password,
+    name,
+    location,
+  });
+  return res.redirect("/login");
 };
 //로그인 Login (Read)
 export const getLogin = (req, res) => {
-  return res.render("join", { pageTitle: "Join" });
+  return res.render("login", { pageTitle: "Login" });
 };
 export const postLogin = (req, res) => {
   return res.redirect("/");
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 //Define the shape(schema) of the Model
 const userSchema = new mongoose.Schema({
@@ -7,6 +8,14 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: { type: String },
+});
+
+//Hash Middleware
+userSchema.pre("save", async function () {
+  //this(==user)의 password를 5번 해싱함
+  console.log("original pw:", this.password); //original pw: 111
+  this.password = await bcrypt.hash(this.password, 5);
+  console.log("hashed pw:", this.password); //hashed pw: $2b$05$nF3r9eefXBKSkq7fyVi5yusHbFIYlJrh1H.tCDe3zgis4dHkDEP7u
 });
 
 //Create the Model with schema
