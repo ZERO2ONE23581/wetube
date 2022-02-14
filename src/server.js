e37315b3e782b244bf173e90255a5ca601716a0e
aiden <zero2one23581@gmail.com>
2022-02-14 15:38:57 +0900

    session 2 - locals

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 //EXPRESS APPLICATION
 const app = express();
@@ -17,20 +18,13 @@ app.use(express.urlencoded({ extended: true })); //enable Express to undertand F
 app.use(logger);
 app.use(
   //session; this middleware will remember everybody who visits my website!
-  ///even if they're not logged in, my website will remember them with the text(cookie) that the backend gives to the browser
   session({
     secret: "hello!",
     resave: true,
     saveUninitialized: true,
   })
 );
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    //session-id is saved with the info added on the userController
-    console.log(sessions);
-    next();
-  });
-});
+app.use(localsMiddleware); //locals
 //Routeres
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
