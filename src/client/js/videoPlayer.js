0c81ae6b363f0f14394372a57c7848301db0100a
aiden <zero2one23581@gmail.com>
2022-02-16 10:37:21 +0900

    volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,6 +3,9 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+//DEFAULT
+let volumeValue = 0.5; //초기 볼륨값 (글로벌 변수)
+video.volume = volumeValue;
 
 //VIDEO PLAY
 //play and pause function
@@ -23,9 +26,26 @@ const handleMute = (e) => {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  //뮤트해제시 마지막설정(last one) 볼륨값으로 회귀
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+//VIDEO VOLUME
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  console.log(event.target.value);
+  //mute시 range조정하면 mute해제 및 text 바뀜
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value; //뮤트해제시 마지막설정(last one) 볼륨값으로 회귀
+  video.volume = value; //인풋과 비디오볼륨값을 연결 => 컨트롤
 };
 
 //EVENT LISTENER
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
