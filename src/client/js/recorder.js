7336f448880e639f8365554b50f5dfe5c5ba6ebe
aiden <zero2one23581@gmail.com>
2022-02-16 16:39:22 +0900

    video preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,11 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
   console.log(stream);
+  video.srcObject = stream; //srcObject는 video에게 무언가주는걸 의미, url사용시 src와는 다르다.
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
