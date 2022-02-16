c83713551b9308af692a781fd247086a293c7800
aiden <zero2one23581@gmail.com>
2022-02-16 19:03:09 +0900

    recap (주석정리)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -6,9 +6,9 @@ let stream;
 let recorder;
 let videoFile;
 
-//녹화다운
+//다운로드
 const handleDownload = () => {
-  //create a link
+  //4. a태그생성 - a주소생성(브라우저url삽입) - 다운로드생성 - html에 a태그 삽입 - 클릭이벤트 넣어줌
   const a = document.createElement("a");
   a.href = videoFile;
   a.download = "MyRecording.webm"; //MyRecording이라는 이름으로 저장할수 있게 해줌, webm은 확장자명 (extension)
@@ -20,34 +20,34 @@ const handleDownload = () => {
 const handleStop = () => {
   startBtn.innerText = "Downlad Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleDownload);
-  //녹화중단
-  recorder.stop();
+  startBtn.addEventListener("click", handleDownload); //4.
+  recorder.stop(); //3. 녹화중단
 };
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  //녹화
-  recorder = new MediaRecorder(stream); //1. 녹음기 생성
-  recorder.start(); //2. 녹화시작
-  //3. 녹화파일 생성
+  //
+  recorder = new MediaRecorder(stream); //1. stream을 받아와서 그걸로 => 녹음기 생성
+  //
+  //3. ondataavailable 녹화가 중단되면 발생하는 event
   recorder.ondataavailable = (event) => {
-    videoFile = URL.createObjectURL(event.data); //메모리url 생성
-    video.src = videoFile; //video 삽입
-    video.srcObject = null; //이전 preview 삭제
-    video.loop = true; //반복
-    video.play(); //재생
-    //녹화된 비디오 catch -> blob 객체 (==event.data) -> 브라우저 메모리상 url 생성(?) (실제로는 없는 url)
-    //실제로 url을 만드는것은 아니고, 브라우저 메모리에 파일을 저장해두고, 브라우저가 그 파일에 접근할수 있는 url을 주는것.
+    videoFile = URL.createObjectURL(event.data); //브라우저가 열려있는 상태에서만 존재
+    video.src = videoFile;
+    video.srcObject = null;
+    video.loop = true;
+    video.play();
   };
+  recorder.start(); //2. 녹화시작
 };
 
 //미리보기 preview
 const init = async () => {
+  //mediaDevices는 말그대로 카메라, 마이크등 접근가능하게함. stream은 0과1로된 데이터.
   stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
-  video.srcObject = stream; //srcObject는 video에게 무언가주는걸 의미, url사용시 src와는 다르다.
-  video.play();
+  //카메라가 stream을 담아와서 video tag에 넣어준것 => 재생순
+  video.srcObject = stream;
+  video.play(); //실시간 스트림
 };
 init(); //함수실행
 
