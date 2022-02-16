fe956854a271825a0f0b5800ad7c9cb8a1cd350c
aiden <zero2one23581@gmail.com>
2022-02-16 09:47:17 +0900

    PLAY PAUSE

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,26 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+//VIDEO PLAY
+//play and pause function
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+//change innertext
+const handlePlay = () => (playBtn.innerText = "Pause");
+const handlePause = () => (playBtn.innerText = "Play");
+
+//VIDEO MUTE
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
