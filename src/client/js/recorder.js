26d019bfbac94967601f197a98b87517e5aa079e
aiden <zero2one23581@gmail.com>
2022-02-17 13:38:30 +0900

    thumbnail upload

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,6 +1,6 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-const startBtn = document.getElementById("startBtn");
+const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
 
 //Global variable
@@ -8,95 +8,102 @@ let stream;
 let recorder;
 let videoFile;
 
-//다운로드
+const files = {
+  input: "recording.webm",
+  output: "output.mp4",
+  thumb: "thumbnail.jpg",
+};
+
+//과정) a태그생성 - a주소생성(브라우저url삽입) - 다운로드 파일이름 생성 - html에 a태그 삽입 - 클릭이벤트 넣어줌
+const downloadFile = (fileUrl, fileName) => {
+  const a = document.createElement("a");
+  a.href = fileUrl; //mp4로 변환된 파일url 삽입 ffmpeg
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click(); //유저가 클릭한것 처럼 작동함
+};
+
+//7. 파일변환 FFMPEG
 const handleDownload = async () => {
+  actionBtn.removeEventListener("click", handleDownload); //다운로드 이벤트 제거
+  actionBtn.innerText = "Transcoding...";
+  actionBtn.disabled = true;
+
   //--------------------------** webm -> mp4 파일변환 **------------------------------//
-  //0. ffmpeg 소프트웨어 application
+  //1. ffmpeg 소프트웨어 설치
   const ffmpeg = createFFmpeg({
     //!!!ffmpeg 최신버전(10.1)의 오류해결방법!!! 아래 corePath추가
     corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
     log: true, //콘솔로 찍힘
   });
-  await ffmpeg.load(); //무거운 소프트웨어이기때문에 await을 사용; 유저의 컴퓨터를 사용하기에 서버를 쓸 필요없음 (서버비용절감)
+  await ffmpeg.load(); //실행
 
-  //1. ffmpeg의 가상세계에 파일을 생성함; videoFile(브라우저url)을 넣어서 실제 파일을 생성.
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  //1. ffmpeg의 가상세계에 파일을 생성
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
 
-  //2. 만든파일을 input으로 받아서 실행함; recording.webm을 input으로 받아서 -> mp4형태로 변환한것; 초당 60프레임으로 인코딩함
+  //2. 만든파일을 input으로 받아서 실행함;
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output); // recording.webm -> output.mp4
+  await ffmpeg.run("-i", files.input, "-ss", "00:00:01", "-frames:v", "1", files.thumb); // thumbnail.jpg
 
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  //3-1. 만든 mp4파일가져옴 (readFile)
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
 
-  //4. 썸네일 만들기
-  await ffmpeg.run(
-    "-i",
-    "recording.webm",
-    "-ss",
-    "00:00:01",
-    "-frames:v",
-    "1",
-    "thumbnail.jpg"
-  );
-  //5. 썸네일 불러오기
-  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
-  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
-  const thumbUrl = URL.createObjectURL(thumbBlob);
-
-  //3. 파일불러오기;
-  /// 과정) mp4파일가져옴 - 파일로부터 data를 받음(blob) - Object URL을 만듬 -> a.href에 넣어줌
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  //3-2. 파일로부터 data를 받음(new Blob)
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+
+  //3-3. Object URL (브라우저내 가상url)을 만듬
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
   //--------------------------** webm -> mp4 파일변환 **------------------------------//
 
-  //4. a태그생성 - a주소생성(브라우저url삽입) - 다운로드생성 - html에 a태그 삽입 - 클릭이벤트 넣어줌
-  const a = document.createElement("a");
-  a.href = mp4Url; //mp4로 변환된 파일url 삽입 ffmpeg
-  a.download = "MyRecording.mp4";
-  document.body.appendChild(a);
-  a.click(); //유저가 클릭한것 처럼 작동함
-
-  // 썸네일 생성
-  const thumbA = document.createElement("a");
-  thumbA.href = thumbUrl; //썸네일 url 삽입 ffmpeg
-  thumbA.download = "MyThumbnail.jpg";
-  document.body.appendChild(thumbA);
-  thumbA.click(); //유저가 클릭한것 처럼 작동함
+  //8. 다운로드
+  downloadFile(mp4Url, "MyRecording.mp4");
+  downloadFile(thumbUrl, "MyThumbnail.jpg");
 
   //** ffmpeg 속도향상작업 **
   //파일제거
-  ffmpeg.FS("unlink", "recording.webm");
-  ffmpeg.FS("unlink", "output.mp4");
-  ffmpeg.FS("unlink", "thumbnail.jpg");
+  ffmpeg.FS("unlink", files.input);
+  ffmpeg.FS("unlink", files.output);
+  ffmpeg.FS("unlink", files.thumb);
   // url제거
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
   URL.revokeObjectURL(videoFile);
+
+  //8. 다운받은후 다시 복귀
+  actionBtn.disabled = false;
+  actionBtn.innerText = "Record Again";
+  actionBtn.addEventListener("click", handleStart); //Record Again 클릭!
 };
 
 //녹화 recording
 const handleStop = () => {
-  startBtn.innerText = "Downlad Recording";
-  startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleDownload); //4.
-  recorder.stop(); //3. 녹화중단
+  actionBtn.innerText = "Download Recording";
+  actionBtn.removeEventListener("click", handleStop);
+  actionBtn.addEventListener("click", handleDownload); //6. Download Recording 클릭!
+  recorder.stop(); //5. 녹화중단
 };
 const handleStart = () => {
-  startBtn.innerText = "Stop Recording";
-  startBtn.removeEventListener("click", handleStart);
-  startBtn.addEventListener("click", handleStop);
-  //
-  recorder = new MediaRecorder(stream); //1. stream을 받아와서 그걸로 => 녹음기 생성
-  //
-  //3. ondataavailable 녹화가 중단되면 발생하는 event
+  actionBtn.innerText = "Stop Recording";
+  actionBtn.removeEventListener("click", handleStart);
+  actionBtn.addEventListener("click", handleStop); //4. Stop Recording 클릭!
+
+  //1. stream을 받아와서 그걸로 => 녹음기 생성
+  recorder = new MediaRecorder(stream);
+
+  //2. 녹음준비
   recorder.ondataavailable = (event) => {
+    // 녹화가 중단되면 발생하는 event
     videoFile = URL.createObjectURL(event.data); //브라우저가 열려있는 상태에서만 존재
     video.src = videoFile;
     video.srcObject = null;
     video.loop = true;
     video.play();
   };
-  recorder.start(); //2. 녹화시작
+  recorder.start(); //3. 녹화시작
 };
 
 //미리보기 preview
@@ -109,4 +116,4 @@ const init = async () => {
 };
 init(); //함수실행
 
-startBtn.addEventListener("click", handleStart);
+actionBtn.addEventListener("click", handleStart);
--- src/models/Video.js
+++ src/models/Video.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 //Define the shape(schema) of the Model
 const videoSchema = new mongoose.Schema({
-  fileUrl: { type: String, required: true },
+  fileUrl: { type: String, required: true }, //녹화비디오 mp4 url
+  thumbUrl: { type: String, required: true }, //썸네일 jpg파일 url
   title: { type: String, required: true, trim: true, maxlength: 50, uppercase: true },
   description: { type: String, required: true, trim: true, maxlength: 50 },
   createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String, trim: true }], //hashtags는 복수임으로 array 형태
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
