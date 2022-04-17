fb6f317d65a2bf2bef1def18b86a4949d134bc64
aiden <zero2one23581@gmail.com>
2022-04-18 00:24:34 +0900

    backend 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const BASE_JS = "./src/client/js/";
+const BASE_JS = './src/client/js/';
 
 module.exports = {
   //entry means source code (in this case main.js)
   ///entry is the file we'd like to transfrom from new js -> old ugly compressed version of js
   entry: {
-    main: BASE_JS + "main.js",
-    videoPlayer: BASE_JS + "videoPlayer.js",
-    recorder: BASE_JS + "recorder.js",
-    commentSection: BASE_JS + "commentSection.js",
+    main: BASE_JS + 'main.js',
+    videoPlayer: BASE_JS + 'videoPlayer.js',
+    recorder: BASE_JS + 'recorder.js',
+    commentSection: BASE_JS + 'commentSection.js',
   },
-  mode: "development",
-  watch: true, //this prevent "watching client console"(npm run assets) to stop running
   plugins: [
     new MiniCssExtractPlugin({
       //this separates styles.css from js file
-      filename: "css/styles.css",
+      filename: 'css/styles.css',
     }),
   ],
   output: {
-    filename: "js/[name].js",
-    path: path.resolve(__dirname, "assets"), // this is where the transformed file is saved.
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'assets'), // this is where the transformed file is saved.
     clean: true, //clean output folder before you build frontEnd
   },
   module: {
@@ -30,15 +28,15 @@ module.exports = {
       {
         test: /\.js$/, //we're taking all the js files and make transforms
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
           },
         },
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
