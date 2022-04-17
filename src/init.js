c385e3b505e07a00e2179e26b19ae277e4e453cc
aiden <zero2one23581@gmail.com>
2022-04-18 00:12:34 +0900

    backend 2

--- src/init.js
+++ src/init.js
@@ -1,15 +1,18 @@
-import "dotenv/config"; //you should import dotenv to activate on top of your code!
-import "./db"; // server import this file to connect the server to mongodb
+import 'regenerator-runtime';
+import 'dotenv/config'; //you should import dotenv to activate on top of your code!
+import './db'; // server import this file to connect the server to mongodb
 
 //This connects db file to mongoose => let db to notice there's a Model!
 ///When this line is created, you can see you have wetube db on mongodb!!
-import "./models/Video";
-import "./models/User";
-import "./models/Comment";
+import './models/Video';
+import './models/User';
+import './models/Comment';
 
 //SERVER LISTENING
-import app from "./server";
+import app from './server';
 const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`✅ Aiden's server listening on the port http://localhost:${PORT} 🔥`);
+  console.log(
+    `✅ Aiden's server listening on the port http://localhost:${PORT} 🔥`
+  );
 });
